35696c3fdc97228352103ff532628f295abe10a2
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-01 18:10:26 -0300

    Update ViewConsole.cs
    Add color to logs

--- MultiplayerServer/ServerView/src/ViewConsole.cs
+++ MultiplayerServer/ServerView/src/ViewConsole.cs
@@ -12,6 +12,11 @@ namespace ServerView.src
 {
     internal class ViewConsole : IDisposable, IInitable
     {
+        const string RED = "\x1B[31m";
+        const string YELLOW = "\x1B[33m";
+        const string GREEN = "\x1B[32m";
+        const string RESET = "\x1B[0m";
+
         private EventBus EventBus => ServiceProvider.Instance.GetService<EventBus>();
 
         public ViewConsole()
@@ -31,17 +36,17 @@ namespace ServerView.src
 
         private void LogMessage(in ConsoleLogEvent consoleLogEvent)
         {
-            Console.WriteLine($"[LOG]: {consoleLogEvent.message}.\n");
+            Console.WriteLine($"{GREEN}[LOG]{RESET}: {consoleLogEvent.message}.\n");
         }
 
         private void LogWarning(in ConsoleWarningEvent consoleWarningEvent)
         {
-            Console.WriteLine($"[WARNING]: {consoleWarningEvent.message}.\n");
+            Console.WriteLine($"{YELLOW}[WARNING]{RESET}: {consoleWarningEvent.message}.\n");
         }
 
         private void LogError(in ConsoleErrorEvent consoleErrorEvent)
         {
-            Console.WriteLine($"[ERROR]: {consoleErrorEvent.message}.\n");
+            Console.WriteLine($"{RED}[ERROR]{RESET}: {consoleErrorEvent.message}.\n");
         }
 
         public void Dispose()
