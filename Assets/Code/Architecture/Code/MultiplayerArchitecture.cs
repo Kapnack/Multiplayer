a64e0539756f9950aefbd8073ce5a7e0b5f0945a
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-06 17:37:47 -0300

    Update MultiplayerArchitecture.cs

--- Assets/Code/Architecture/Code/MultiplayerArchitecture.cs
+++ Assets/Code/Architecture/Code/MultiplayerArchitecture.cs
@@ -1,26 +1,32 @@
+using Assets.Code.Architecture.Code.Client;
 using ImageCampus.ToolBox.Dataflow;
+using ImageCampus.ToolBox.Services;
 using System;
 
 public class MultiplayerArchitecture : IInitable, ITickable, IDisposable
 {
+    GameClient GameClient => ServiceProvider.Instance.GetService<GameClient>();
+
     public void Init()
     {
+        ServiceProvider.Instance.AddService<GameClient>(new GameClient());
 
+        GameClient.Init();
     }
 
     public void LateInit()
     {
-
+        GameClient.LateInit();
     }
 
     public void Tick(float deltaTime)
     {
-
+        GameClient.Tick(deltaTime);
     }
 
     public void Dispose()
     {
-
+        GameClient.Dispose();
     }
 
 }
