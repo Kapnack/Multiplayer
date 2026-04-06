82015f1c95f74a70d381bb643fc0e051dad640ee
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-06 16:14:10 -0300

    Create Unity Architecture Base

---
+++ Assets/Code/MultiplayerArchitecture/Code/MultiplayerArchitecture.cs
@@ -0,0 +1,26 @@
+using ImageCampus.ToolBox.Dataflow;
+using System;
+
+public class MultiplayerArchitecture : IInitable, ITickable, IDisposable
+{
+    public void Init()
+    {
+
+    }
+
+    public void LateInit()
+    {
+
+    }
+
+    public void Tick(float deltaTime)
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+}
