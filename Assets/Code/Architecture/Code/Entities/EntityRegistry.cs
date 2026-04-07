2dee0701c1721f3530180f1c4ee3c37c583b08e0
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-07 02:36:09 -0300

    Create EntityRegistry.cs

---
+++ Assets/Code/Architecture/Code/Entities/EntityRegistry.cs
@@ -0,0 +1,48 @@
+﻿using ImageCampus.ToolBox.Events;
+using ImageCampus.ToolBox.Services;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Assets.Code.Architecture.Code.Entities
+{
+    internal class EntityRegistry : IService, IDisposable
+    {
+
+        public bool IsPersistance => false;
+
+        internal string RegisterMethodName => nameof(Register);
+
+        private Dictionary<uint, Entity> entities;
+        private Dictionary<Type, List<uint>> entitiesIDsByType;
+
+        public EntityRegistry()
+        {
+            entities = new Dictionary<uint, Entity>();
+            entitiesIDsByType = new Dictionary<Type, List<uint>>();
+        }
+
+        private void Register(Entity entity)
+        {
+            entities.Add(entity.ID, entity);
+            Type currentEntityType = null;
+
+            do
+            {
+                currentEntityType = currentEntityType == null ? entity.GetType() : currentEntityType.BaseType;
+
+                if (!entitiesIDsByType.ContainsKey(currentEntityType))
+                    entitiesIDsByType.Add(currentEntityType, new List<uint>());
+
+                entitiesIDsByType[currentEntityType].Add(entity.ID);
+
+            } while (currentEntityType != typeof(Entity));
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
