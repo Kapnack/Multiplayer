957863a17bca00f6def9b1416c0fca8dff91ec31
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-07 02:36:26 -0300

    Create EntityCreatedEvent.cs

---
+++ Assets/Code/Architecture/Code/Entities/Events/EntityCreatedEvent.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Code.Architecture.Code.Math;
+using ImageCampus.ToolBox.Events;
+
+namespace Assets.Code.Architecture.Code.Entities.Events
+{
+    public struct EntityCreatedEvent<EntityType> : IEvent where EntityType : Entity
+    {
+        public uint entityID;
+        public Coordinate position;
+
+        public void Assign(params object[] parameters)
+        {
+            entityID = (uint)parameters[0];
+            position = (Coordinate)parameters[1];
+        }
+
+        public void Reset()
+        {
+            entityID = default(uint);
+            position = default(Coordinate);
+        }
+    }
+}
