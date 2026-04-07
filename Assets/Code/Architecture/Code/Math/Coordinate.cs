fba9f46c55405719a9c050e7237925f4865a6574
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-07 02:52:19 -0300

    Create Coordinate.cs

---
+++ Assets/Code/Architecture/Code/Math/Coordinate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Code.Architecture.Code.Math
+{
+    public struct Coordinate
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Coordinate(float x, float y, float z)
+        {
+            this.x = x; 
+            this.y = y; 
+            this.z = z;
+        }
+
+        static public Coordinate Zero => new Coordinate(0, 0, 0);
+    }
+}
