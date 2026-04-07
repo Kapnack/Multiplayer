2e224077c3907b346df4fdc2c60d75f99c8c50ee
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-07 02:36:32 -0300

    Create SpawnRequestAcceptedEvent.cs

---
+++ Assets/Code/Architecture/Code/Entities/Events/SpawnRequestAcceptedEvent.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Code.Architecture.Code.Math;
+using ImageCampus.ToolBox.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Code.Architecture.Code.Entities.Events
+{
+    public struct SpawnRequestAcceptedEvent<EntityType> : IEvent
+    {
+        public string blueprintToSpawn;
+        public Coordinate coordinateToSpawn;
+        public string blueprintTable;
+
+        public void Assign(params object[] parameters)
+        {
+            blueprintToSpawn = (string)parameters[0];
+            coordinateToSpawn = (Coordinate)parameters[1];
+            blueprintTable = (string)parameters[2];
+        }
+
+        public void Reset()
+        {
+            blueprintToSpawn = string.Empty;
+            coordinateToSpawn = default(Coordinate);
+            blueprintTable = default(string);
+        }
+    }
+
+    public struct SpawnRequestAcceptedEvent : IEvent
+    {
+        public string blueprintToSpawn;
+        public Coordinate coordinateToSpawn;
+        public string blueprintTable;
+        public string entityTypeName;
+
+        public void Assign(params object[] parameters)
+        {
+            blueprintToSpawn = (string)parameters[0];
+            coordinateToSpawn = (Coordinate)parameters[1];
+            blueprintTable = (string)parameters[2];
+            entityTypeName = (string)parameters[3];
+        }
+
+        public void Reset()
+        {
+            blueprintToSpawn = string.Empty;
+            coordinateToSpawn = default(Coordinate);
+            blueprintTable = default(string);
+            entityTypeName = default(string);
+        }
+    }
+}
