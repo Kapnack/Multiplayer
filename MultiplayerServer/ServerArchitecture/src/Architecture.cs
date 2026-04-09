9c10fc7e0f5c8f5f0f0dd6cca3d4139d991777a1
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-08 21:29:00 -0300

    Revert "Fix Projects"
    This reverts commit c322d3adba363beb058a6baf0b0a58a3b6fa5804.

---
+++ MultiplayerServer/ServerArchitecture/src/Architecture.cs
@@ -0,0 +1,49 @@
+﻿using ImageCampus.ToolBox.Dataflow;
+using ImageCampus.ToolBox.Events;
+using ImageCampus.ToolBox.Services;
+using ImageCampus.ToolBox.Scheduling;
+using KapNet;
+using ServerArquitecture.src;
+using System;
+
+namespace MultiplayerServer.src
+{
+    public sealed class Architecture : IInitable, IDisposable, ITickable
+    {
+        private Server server;
+
+        TaskScheduler TaskScheduler => ServiceProvider.Instance.GetService<TaskScheduler>();
+        Time Time => ServiceProvider.Instance.GetService<Time>();
+
+        public void Init()
+        {
+            ServiceProvider.Instance.AddService<Time>(new Time());
+            ServiceProvider.Instance.AddService<TaskScheduler>(new TaskScheduler());
+            ServiceProvider.Instance.AddService<EventBus>(new EventBus());
+            ServiceProvider.Instance.AddService<PacketFactory>(new PacketFactory());
+
+            server = new Server();
+
+            Time.Init();
+            server.Init();
+        }
+
+        public void LateInit()
+        {
+            Time.LateInit();
+            server.LateInit();
+        }
+
+        public void Tick(float deltaTime)
+        {
+            server.Tick(deltaTime);
+            TaskScheduler.Tick(deltaTime);
+            Time.Tick();
+        }
+
+        public void Dispose()
+        {
+            server.Dispose();
+        }
+    }
+}
