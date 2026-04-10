1d49bbe8f8f861b62eb40e1b7fc7bd5424d66307
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-10 01:59:48 -0300

    Update PacketFactory.cs

--- MultiplayerServer/Architecture/src/Server/Packets/PacketFactory.cs
+++ MultiplayerServer/Architecture/src/Server/Packets/PacketFactory.cs
@@ -24,7 +24,8 @@ namespace KapNet
         None = 1 << 0,
         Crytical = 1 << 1,
         Reliable = 1 << 2,
-        Encrypted = 1 << 3
+        Encrypted = 1 << 3,
+        Ordenable = 1 << 4
     }
 
     internal class PacketFactory : IService
