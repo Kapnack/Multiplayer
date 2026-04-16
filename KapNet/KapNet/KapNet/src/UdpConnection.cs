8ba0b442d805c42045cf9a473e434da170d75bdb
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-16 05:14:05 -0300

    Update UdpConnection.cs
    Replace old UdpClient.BegingRecieve for UdpClient.RecieveAsyn to fix Client Force close, improve loop structure and allow an easy threading

--- KapNet/KapNet/KapNet/src/UdpConnection.cs
+++ KapNet/KapNet/KapNet/src/UdpConnection.cs
@@ -1,83 +1,82 @@
+using KapNet;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-
-namespace KapNet
+public class UdpConnection
 {
-    public class UdpConnection
+    private struct DataReceived
     {
-        private struct DataReceived
+        public byte[] data;
+
+        public IPEndPoint ipEndPoint;
+    }
+
+    private readonly UdpClient connection;
+
+    private IReceiveData receiver = null;
+
+    private bool isRunning = true;
+
+    private Queue<DataReceived> dataReceivedQueue = new Queue<DataReceived>();
+
+    private Thread recieveData;
+
+    public UdpConnection(int port, IReceiveData receiver = null)
+    {
+        connection = new UdpClient(port);
+        this.receiver = receiver;
+        recieveData = new Thread(StartReceiveLoop);
+        recieveData.Start();
+    }
+
+    public UdpConnection(IPAddress ip, int port, IReceiveData receiver = null)
+    {
+        connection = new UdpClient();
+        Connect(ip, port);
+        this.receiver = receiver;
+        recieveData = new Thread(StartReceiveLoop);
+        recieveData.Start();
+    }
+
+    private void Connect(IPAddress ip, int port)
+    {
+        connection.Connect(ip, port);
+    }
+
+    public void Close()
+    {
+        isRunning = false;
+        recieveData.Abort();
+        connection.Close();
+    }
+
+    public void FlushReceiveData()
+    {
+        lock (dataReceivedQueue)
         {
-            public byte[] data;
-            public IPEndPoint ipEndPoint;
-        }
-
-        private readonly UdpClient connection;
-        private IReceiveData receiver = null;
-        private bool isRunning = true;
-        private Queue<DataReceived> dataReceivedQueue = new Queue<DataReceived>();
-
-        Thread recieveData;
-
-        public UdpConnection(int port, IReceiveData receiver = null)
-        {
-            connection = new UdpClient(port);
-
-            this.receiver = receiver;
-
-            recieveData = new Thread(StartReceiveLoop);
-            recieveData.Start();
-        }
-
-        public UdpConnection(IPAddress ip, int port, IReceiveData receiver = null)
-        {
-            connection = new UdpClient();
-            Connect(ip, port);
-
-            this.receiver = receiver;
-
-            recieveData = new Thread(StartReceiveLoop);
-            recieveData.Start();
-        }
-
-        private void Connect(IPAddress ip, int port)
-        {
-            connection.Connect(ip, port);
-        }
-
-        public void Close()
-        {
-            isRunning = false;
-            recieveData.Abort();
-            connection.Close();
-        }
-
-        public void FlushReceiveData()
-        {
-            lock (dataReceivedQueue)
+            while (dataReceivedQueue.Count > 0)
             {
-                while (dataReceivedQueue.Count > 0)
+                DataReceived dataReceived = dataReceivedQueue.Dequeue();
+                if (receiver != null)
                 {
-                    DataReceived dataReceived = dataReceivedQueue.Dequeue();
-                    if (receiver != null)
-                        receiver.OnReceiveData(dataReceived.data, dataReceived.ipEndPoint);
+                    receiver.OnReceiveData(dataReceived.data, dataReceived.ipEndPoint);
                 }
             }
         }
+    }
 
-        public async void StartReceiveLoop()
+    public async void StartReceiveLoop()
+    {
+        while (isRunning)
         {
-            while (isRunning)
+            try
             {
-                try
+                UdpReceiveResult result = await connection.ReceiveAsync();
+
+                if (IsValidCheckSum(result.Buffer))
                 {
-                    UdpReceiveResult result = await connection.ReceiveAsync();
-
-                    if (!IsValidCheckSum(result.Buffer))
-                        continue;
-
                     lock (dataReceivedQueue)
                     {
                         dataReceivedQueue.Enqueue(new DataReceived
@@ -87,31 +86,62 @@ namespace KapNet
                         });
                     }
                 }
-                catch (ObjectDisposedException)
-                {
-                    break;
-                }
-                catch (Exception)
-                {
-                    break;
-                }
+            }
+            catch (ObjectDisposedException)
+            {
+                break;
+            }
+            catch (Exception)
+            {
+                break;
             }
         }
+    }
 
-        private bool IsValidCheckSum(byte[] data)
-        {
-            return PacketUtility.CalculateCheckSum(data, 0, PacketLayout.CheckSum1EndOffSet) == PacketUtility.GetCheckSum1(data) &&
-                PacketUtility.CalculateCheckSum(data, 0, PacketLayout.CheckSum2EndOffSet) == PacketUtility.GetCheckSum2(data);
-        }
+    //void OnReceive(IAsyncResult ar)
+    //{
+    //    try
+    //    {
+    //        DataReceived dataReceived = new DataReceived();
+    //        dataReceived.data = connection.EndReceive(ar, ref dataReceived.ipEndPoint);
+    //
+    //        if (!IsValidCheckSum(dataReceived.data))
+    //        {
+    //            return;
+    //        }
+    //
+    //        lock (dataReceivedQueue)
+    //        {
+    //            dataReceivedQueue.Enqueue(dataReceived);
+    //        }
+    //    }
+    //    catch (SocketException e)
+    //    {
+    //        
+    //    }
+    //    catch (Exception e)
+    //    {
+    //       
+    //    }
+    //    finally
+    //    {
+    //        if (isRunning)
+    //            connection.BeginReceive(OnReceive, null);
+    //    }
+    //}
 
-        public void Send(byte[] data)
-        {
-            connection.Send(data, data.Length);
-        }
+    private bool IsValidCheckSum(byte[] data)
+    {
+        return PacketUtility.CalculateCheckSum(data, 0, 8) == PacketUtility.GetCheckSum1(data) && PacketUtility.CalculateCheckSum(data, 0, 4) == PacketUtility.GetCheckSum2(data);
+    }
 
-        public void Send(byte[] data, IPEndPoint ipEndPoint)
-        {
-            connection.Send(data, data.Length, ipEndPoint);
-        }
+    public void Send(byte[] data)
+    {
+        connection.Send(data, data.Length);
+    }
+
+    public void Send(byte[] data, IPEndPoint ipEndPoint)
+    {
+        connection.Send(data, data.Length, ipEndPoint);
     }
 }
