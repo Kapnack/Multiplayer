6023485eb0d1002652bb811ed999e1c370ada1b6
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 16:59:03 -0300

    Create BaseScene.cs

---
+++ Assets/MultiplayerArchitecture/Code/Scenes/BaseScene.cs
@@ -0,0 +1,14 @@
+﻿using ImageCampus.ToolBox.Dataflow;
+using System;
+
+namespace MultiplayerArchitecture.Code.Scenes
+{
+    public abstract class BaseScene : IInitable, ITickable, IDisposable
+    {
+        public abstract void Init();
+        public abstract void Init(params object[] parameters);
+        public abstract void LateInit();
+        public abstract void Tick(float deltaTime);
+        public abstract void Dispose();
+    }
+}
