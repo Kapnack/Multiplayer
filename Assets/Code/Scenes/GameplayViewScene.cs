bf5d5563b464cee703fe0fdf46a17790502e9be9
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 17:02:26 -0300

    Create GameplayViewScene.cs

---
+++ Assets/Code/Scenes/GameplayViewScene.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Code.Entities;
+using ImageCampus.ToolBox.Services;
+using MultiplayerArchitecture.Code.Scenes;
+using UnityEngine;
+
+namespace Assets.Code.Scenes
+{
+    internal class GameplayViewScene : BaseScene
+    {
+        private EntityFactoryView entityFactoryView;
+        private EntityLogicView entityLogicView;
+
+        public override void Init()
+        {
+        }
+
+        public override void Init(params object[] parameters)
+        {
+            Init();
+
+            ServiceProvider.Instance.AddService<EntityRegistryView>(new EntityRegistryView());
+            entityFactoryView = new EntityFactoryView((GameObject)parameters[0], (Camera)parameters[1]);
+            entityLogicView = new EntityLogicView();
+
+            entityFactoryView.Init();
+            entityLogicView.Init();
+        }
+
+        public override void LateInit()
+        {
+            entityFactoryView.LateInit();
+            entityLogicView.LateInit();
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            
+        }
+
+        public override void Dispose()
+        {
+            entityLogicView.Dispose();
+            entityFactoryView.Dispose();
+            ServiceProvider.Instance.RemoveService<EntityRegistryView>();
+        }
+    }
+}
