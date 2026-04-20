42267cbb1f0cb71a87cf905b06fc9c2965a45313
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 17:02:59 -0300

    Update MultiplayerArchitecture.cs

--- Assets/MultiplayerArchitecture/Code/MultiplayerArchitecture.cs
+++ Assets/MultiplayerArchitecture/Code/MultiplayerArchitecture.cs
@@ -1,46 +1,35 @@
-
-using Assets.MultiplayerArchitecture.Code.Entities;
-using Assets.MultiplayerArchitecture.Code.Network;
 using ImageCampus.ToolBox.Dataflow;
 using ImageCampus.ToolBox.Events;
 using ImageCampus.ToolBox.Services;
 using System;
+using MultiplayerArchitecture.Code.Scenes;
 
 namespace Multiplayer.Arch
 {
     public class MultiplayerArchitecture : IInitable, ITickable, IDisposable
     {
-        GameClient GameClient => ServiceProvider.Instance.GetService<GameClient>();
-        EntityLogic entityLogic = new EntityLogic();
+        BaseScene currentScene;
 
         public void Init()
         {
             ServiceProvider.Instance.AddService<EventBus>(new EventBus());
-            ServiceProvider.Instance.AddService<GameClient>(new GameClient());
-            ServiceProvider.Instance.AddService<EntityRegistry>(new EntityRegistry());
-            ServiceProvider.Instance.AddService<EntityFactory>(new EntityFactory());
-
-            GameClient.Init();
-            entityLogic.Init();
-
+            currentScene = new GameplayScene();
+            currentScene.Init();
         }
 
         public void LateInit()
         {
-            GameClient.LateInit();
-            entityLogic.LateInit();
+            currentScene.LateInit();
         }
 
         public void Tick(float deltaTime)
         {
-            GameClient.Tick(deltaTime);
-            entityLogic.Tick(deltaTime);
+            currentScene.Tick(deltaTime);
         }
 
         public void Dispose()
         {
-            entityLogic.Dispose();
-            GameClient.Dispose();
+            currentScene.Dispose();
         }
     }
 }
