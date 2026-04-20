25dd5fb1884eee1e65c7c2740f178a2ebf447b4b
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 16:59:19 -0300

    Update PlayerController.cs

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -5,28 +5,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private const float friction = 4.0f;
-    private const float aceleration = 15.0f;
-
-    private Vector3 velocity;
-
     EventBus EventBus => ServiceProvider.Instance.GetService<EventBus>();
 
-    void Update()
+    [SerializeField] private float acceleration = 20.0f;
+    [SerializeField] private float maxSpeed = 20.0f;
+    [SerializeField] private float turnSpeed = 120.0f;
+    [SerializeField] private float drag = 0f;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+    }
 
-        Vector3 input = new Vector3(h, 0, v);
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+        LimitSpeed();
+        SendMovementEvent();
+    }
 
-        velocity += input * (aceleration * Time.deltaTime);
+    private void Move()
+    {
+        float input = Input.GetAxis("Vertical");
 
-        velocity -= velocity * (friction * Time.deltaTime);
-
-        if (velocity.magnitude > Mathf.Epsilon || Mathf.Approximately(velocity.magnitude, 0))
+        if (input != 0f)
         {
-            Vector3 newPos = transform.position + velocity * Time.deltaTime;
+            rb.AddForce(transform.forward * input * acceleration, ForceMode.Acceleration);
+        }
+
+        rb.linearDamping = drag;
+    }
+
+    private void Turn()
+    {
+        float input = Input.GetAxis("Horizontal");
+
+        if (rb.linearVelocity.magnitude > 0.1f)
+        {
+            float speedFactor = rb.linearVelocity.magnitude / maxSpeed;
+            float turn = input * turnSpeed * speedFactor * Time.fixedDeltaTime;
+
+            Quaternion rot = Quaternion.Euler(0f, turn, 0f);
+            rb.MoveRotation(rb.rotation * rot);
+        }
+    }
+
+    private void LimitSpeed()
+    {
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+    }
+
+    private void SendMovementEvent()
+    {
+        Vector3 velocity = rb.linearVelocity;
+
+        if (velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            Vector3 newPos = transform.position + velocity * Time.fixedDeltaTime;
+
             EventBus.Raise<PlayerMove>(new Coordinate(newPos.x, newPos.y, newPos.z));
         }
     }
+
 }
