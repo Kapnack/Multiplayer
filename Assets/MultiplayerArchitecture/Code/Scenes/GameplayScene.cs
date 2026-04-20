1e323ae8c10a97672eb1dcbb3f52c69e34079200
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 17:03:13 -0300

    Create GameplayScene.cs

---
+++ Assets/MultiplayerArchitecture/Code/Scenes/GameplayScene.cs
@@ -0,0 +1,49 @@
+﻿using Assets.MultiplayerArchitecture.Code.Entities;
+using Assets.MultiplayerArchitecture.Code.Network;
+using ImageCampus.ToolBox.Services;
+
+namespace MultiplayerArchitecture.Code.Scenes
+{
+    internal class GameplayScene : BaseScene
+    {
+        GameClient GameClient => ServiceProvider.Instance.GetService<GameClient>();
+        EntityLogic entityLogic;
+
+        public override void Init()
+        {
+            ServiceProvider.Instance.AddService<GameClient>(new GameClient());
+            ServiceProvider.Instance.AddService<EntityRegistry>(new EntityRegistry());
+            ServiceProvider.Instance.AddService<EntityFactory>(new EntityFactory());
+
+            entityLogic = new EntityLogic();
+
+            GameClient.Init();
+            entityLogic.Init();
+        }
+
+        public override void LateInit()
+        {
+            GameClient.LateInit();
+            entityLogic.LateInit();
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            GameClient.Tick(deltaTime);
+            entityLogic.Tick(deltaTime);
+        }
+
+        public override void Dispose()
+        {
+            entityLogic.Dispose();
+            GameClient.Dispose();
+
+            ServiceProvider.Instance.ClearAllNonPersistanceServices();
+        }
+
+        public override void Init(params object[] parameters)
+        {
+
+        }
+    }
+}
