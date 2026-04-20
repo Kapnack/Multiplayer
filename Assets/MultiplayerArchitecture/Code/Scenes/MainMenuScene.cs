a6cabbe105c175bc3b7a15b04901c2c212d5821d
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-04-20 17:03:24 -0300

    Create MainMenuScene.cs

---
+++ Assets/MultiplayerArchitecture/Code/Scenes/MainMenuScene.cs
@@ -0,0 +1,31 @@
+﻿using MultiplayerArchitecture.Code.Scenes;
+
+namespace Assets.MultiplayerArchitecture.Code.Scenes
+{
+    internal class MainMenuScene : BaseScene
+    {
+        public override void Init()
+        {
+           
+        }
+
+        public override void LateInit()
+        {
+            
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            
+        }
+
+        public override void Dispose()
+        {
+            
+        }
+
+        public override void Init(params object[] parameters)
+        {
+        }
+    }
+}
