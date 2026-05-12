52b3724021c104ba60b82c5a0f8fc96f7cd71347
Kapnack <unknowrealperson@gmail.com>
2026-05-11 21:32:22 -0300

    Create ChangeSceneEvent

---
+++ Assets/MultiplayerArchitecture/Code/Scenes/ChangeSceneEvent.cs
@@ -0,0 +1,26 @@
+﻿using ImageCampus.ToolBox.Events;
+
+namespace MultiplayerArchitecture
+{
+    public enum Scene
+    {
+        None,
+        MainMenu,
+        Gameplay
+    }
+
+    public struct ChangeSceneEvent : IEvent
+    {
+        public Scene scene;
+
+        public void Assign(params object[] parameters)
+        {
+            scene = (Scene)parameters[0];
+        }
+
+        public void Reset()
+        {
+            scene = default(Scene);
+        }
+    }
+}
