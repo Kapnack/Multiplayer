16e6e9b30e5aecd901dcb947582af52d240c2371
Kapnack <unknowrealperson@gmail.com>
2026-05-11 21:31:18 -0300

    Create Service MapToLoad

---
+++ Assets/Code/Scenes/MapToLoad.cs
@@ -0,0 +1,17 @@
+﻿using ImageCampus.ToolBox.Services;
+
+namespace Assets.Code.Scenes
+{
+    public enum Maps
+    {
+        Forest,
+        Snow
+    }
+
+    class MapToLoad : IService
+    {
+        public bool IsPersistance => true;
+
+        public Maps mapToLoad;
+    }
+}
